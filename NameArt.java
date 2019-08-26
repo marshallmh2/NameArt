e206dd9f16ce56b47f6dabf61c6e35042362fc6c
marshallmh2 <54367304+marshallmh2@users.noreply.github.com>
2019-08-26 19:31:12 -0400

    Add files via upload

---
+++ NameArt.java
@@ -0,0 +1,28 @@
+/****************
+ *Name Art
+ ***********************
+ *My name written in large letters and special characters
+ *_______________________
+ * Miles Marshall
+ *08/29/19
+ * CMSC 255 Section 003
+ *************************/
+ 
+public class NameArt
+{
+	
+  public static void main(String [] args) { 
+	System.out.println("Miles Marshall");
+	System.out.println("Project 1-Name in code");
+    System.out.println("$        $  $  $      $$$$$  $$$$$$ ");
+	System.out.println("$$      $$  $  $      $      $      ");
+    System.out.println("$ $    $ $  $  $      $$$    $$$$$$ ");
+    System.out.println("$  $  $  $  $  $      $           $ ");
+    System.out.println("$   $    $  $  $$$$$  $$$$$  $$$$$$ ");
+	System.out.println("08/29/19");
+	System.out.println("CMSC 255");
+  }	  
+	
+
+
+}
